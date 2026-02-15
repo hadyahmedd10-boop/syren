62a7849fca8d73d8a301cbc0a2bbbed5f611106f
hadyahmedd10-boop <hadyahmedd10@gmail.com>
2026-02-15 22:18:00 +0200

    fix(layout): hide footer only on root (/)

--- src/components/layout/FooterGate.tsx
+++ src/components/layout/FooterGate.tsx
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 
 export default function FooterGate() {
   const pathname = usePathname();
-  const hideOn = new Set(["/home", "/"]);
+  const hideOn = new Set(["/"]);
   if (hideOn.has(pathname)) {
     return null;
   }
